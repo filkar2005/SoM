4fc05bf2502eb4af5fe0fb6d77bf6bc5467d69f5
Filaret <filkar2005@yandex.ru>
2025-04-02 21:47:27 +0300

    Расширен Node, выполнена подготовка к написанию метода сечений

--- Base/Structures.py
+++ Base/Structures.py
@@ -6,39 +6,10 @@ Created on Thu Mar 27 17:18:35 2025
 @author: computer
 """
 
-'''
-Реализовано
-
-Базовые классы для узлов и самих стержней
-Намечен класс стержневой системы
-
-Node содержит:
-    координату
-    силы по осям
-    моменты по осям
-    заделки по осям
-    Метод добавления силы (перезаписи?)
-    Метод добавления момента (перезаписи?)
-    Метод добавления заделки
-
-Beam содержит:
-    стартовый и конечный узел
-    силы по осям
-    длину стержня
-    тип нагрузки по осям
-    метод добавления нагрузки + тип (нужно как-то по другому тип добавлять)
-'''
-
-'''
-Планы
-
-В Node нужно добавить:
-
-В Beam нужно добавить:
-    
-'''
-
 import numpy as np
+import sympy
+
+x = sympy.abc.x
 
 class Node:
     '''Класс узла стержневой системы, силы и моменты содержатся по осям в массивах'''
@@ -46,26 +17,36 @@ class Node:
         self.coord = np.array([x, y, z])
         self.forces = np.array([0, 0, 0])
         self.moments = np.array([0, 0, 0])
-        self.supports = [False, False, False]
     
     def add_force(self, fx: float, fy: float, fz: float): 
         self.forces += np.array([fx, fy, fz])
 
     def add_moment(self, mx: float, my: float, mz: float): 
         self.moments += np.array([mx, my, mz])
+        
+class support(Node): # он же шарнир, если не все support активны
+    '''Класс заделки и шариров, указываются направления в которых теряется подвижность'''
+    def __init__(self, x: int, y: int, z: int, sup_x: bool, sup_y: bool, sup_z: bool):
+        super().__init__(x, y, z)
+        self.supports = [sup_x, sup_y, sup_z]
 
-    def set_support(self, x_fixed: bool, y_fixed: bool, z_fixed: bool):
-        self.supports = [x_fixed, y_fixed, z_fixed]
+class MovablePivot(Node): # подвижность указывается в локальных координатах!
+    '''Класс подвижного шарнира, указываются направления получающие подвижность'''
+    def __init__(self, x: int, y: int, z: int, pivot_y: bool, pivot_z: bool):
+        super().__init__(x, y, z)
+        self.piv_y = pivot_y
+        self.piv_z = pivot_z
 
 
 class Beam:
     '''Класс стержня стержневой системы'''
-    def __init__(self, node_start, node_end):
+    def __init__(self, node_start: Node, node_end: Node):
         self.forces = np.array([0, 0, 0])
         self.forces_types = [None, None, None]
         self.length = np.linalg.norm(node_start.coord - node_end.coord)
         self.node_start = node_start
         self.node_end = node_end
+        self.basis = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     
     def add_force(self, fx: float, fy: float, fz: float, force_type=['rect', 'rect', 'rect']):
         self.forces += np.array([fx, fy, fz])
@@ -75,10 +56,7 @@ class Beam:
 class BeamSystem:
     '''Класс стержневой системы, какая-то дичь, по хорошему надо бы нормально придумать хранение элементов'''
     def __init__(self):
-        self.elements = {}
-        self.name_last = 0 # номер последнего элемента
+        self.elements = []
         
-    def add_element(self, name_parent, element):
-        self.name_last += 1
-        element.node_start = self.elements[name_parent].node_end
-        self.elements[str(self.name_last)] = element
+    def add_element(self, element: Beam):
+        self.elements.add(element)
